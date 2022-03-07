ec46db68c0050665924fc18be55fa76c6c3ac714
the-clam <cllam2002@gmail.com>
2022-03-07 18:23:49 -0500

    complete tutorial 8 q3

--- Tutorials/tut8.s
+++ Tutorials/tut8.s
@@ -8,14 +8,52 @@
     .org    0x0000
 
 _start:
-    movia   sp, 0x007FFFFC
+    movia   sp, 0x007FFFFC      # initialize stack pointer
 
-    movi    r2, -128
-    call    PrintSignedHexByte
+    movia   r2, 0xABCDEF12      # initialize test value to be passed into PrintHexWord
+    call    PrintHexWord
 
 _end:
     break
     br _end
+
+# ------------------------------------------------------------
+
+PrintHexWord:
+    subi    sp, sp, 28      # subtract from stack pointer
+    stw     ra, 24(sp)      
+    stw     r2, 16(sp)      # send value to PHB
+    stw     r3, 12(sp)      # loop amount
+    stw     r4, 8(sp)       # loop counter
+    stw     r5, 4(sp)       # hold val
+    stw     r6, 0(sp)       # shift amount multi
+
+    movi    r3, 4           # set loop amount (4 bytes per word)
+    movi    r4, 0           # loop counter
+
+    mov     r5, r2          # move val into r5
+
+phw_loop:
+    movi    r2, 24          # default "shift" amount (ie. need 8 first 8 bits from first, so shift 32 bits 24 right = getting 8 bits)
+    muli    r6, r4, 8       # in loop order(0, 8, 16, 24)
+    sub    r2, r2, r6      # in loop order(24, 16, 8, 0)
+    srl     r2, r5, r2      # shift left by (24, 16, 8, then 0)
+    andi    r2, r2, 0xFF    # only and the last 8 bits
+    call    PrintHexByte
+    addi    r4, r4, 1
+    blt     r4, r3, phw_loop
+
+    ldw     ra, 24(sp)      
+    ldw     r2, 16(sp)      # send value to PHB
+    ldw     r3, 12(sp)      # loop amount
+    ldw     r4, 8(sp)       # loop counter
+    ldw     r5, 4(sp)       # hold val
+    ldw     r6, 0(sp)       # shift amount multi
+    addi    sp, sp, 28      # add to stack pointer
+
+    ret
+
+
 
 # ------------------------------------------------------------
 
@@ -90,25 +128,3 @@ pc_loop:
     ret
     
 # ------------------------------------------------------------
-
-PrintSignedHexByte:
-    subi    sp, sp, 12
-    stw     ra, 8(sp)
-    stw     r2, 4(sp)
-    stw     r3, 0(sp)
-
-pshb_if:
-    bge     r2, r0, pshb_end_if
-pshb_then:
-    mov     r3, r2
-    movi    r2, '-'
-    call    PrintChar
-    sub     r2, r0, r3
-pshb_end_if:
-    call    PrintHexByte
-    
-    ldw     ra, 8(sp)
-    ldw     r2, 4(sp)
-    ldw     r3, 0(sp)    
-    addi    sp, sp, 12
-    ret
