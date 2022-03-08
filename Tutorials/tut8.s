f493d7816932d32050d0bd44a3f8115903005fef
the-clam <cllam2002@gmail.com>
2022-03-08 17:53:30 -0500

    tutorial 8 complete

--- Tutorials/tut8.s
+++ Tutorials/tut8.s
@@ -10,8 +10,14 @@
 _start:
     movia   sp, 0x007FFFFC      # initialize stack pointer
 
-    movia   r2, 65535      # initialize test value to be passed into PrintHexWord
-    call    PrintHexWord
+    movia   r2, 0x12345678      # initialize test value to be passed into PrintHexWord
+    call    PrintHexWord_Attempt
+
+    movi    r2, '\n'            # make new line
+    call    PrintChar
+
+    movia   r2, 0x12345699      # initialize test value to be passed into PrintHexWord
+    call    PrintHexWord_Solution
 
     movi    r2, '\n'            # make new line
     call    PrintChar
@@ -22,7 +28,37 @@ _end:
 
 # ------------------------------------------------------------
 
-PrintHexWord:
+PrintHexWord_Solution:
+    subi    sp, sp, 12          # subtract from stack pointer
+    stw     ra, 8(sp)           # store return address for nested functions
+    stw     r2, 4(sp)           # input value
+    stw     r3, 0(sp)           # temp value    
+
+    mov     r3, r2              # hold value in r3 (so r2 can be used to pass in parameter to PrintHexByte)
+    srli    r2, r2, 24          # logical shift right 24 to only get top 8 bits (srli inserts zeros)
+    call    PrintHexByte        # print first 2 chars (X X _ _ _ _ _ _)
+    mov     r2, r3              # reload value in r2
+    srli    r2, r2, 16          # logical shift right 16 to only get top 16 bits
+    andi    r2, r2, 0xFF        # and with 0xFF to only get the next 2 chars
+    call    PrintHexByte        # print next 2 chars (_ _ X X _ _ _ _)
+    mov     r2, r3              # reload value in r2
+    srli    r2, r2, 8           # logical shift right 8 to only get top 24 bits
+    andi    r2, r2, 0xFF        # and with 0xFF to only get the next 2 chars
+    call    PrintHexByte        # print next 2 chars (_ _ _ _ X X _ _)
+    mov     r2, r3              # reload value in r2 (all 32 bits will be present)
+    andi    r2, r2, 0xFF        # and with 0xFF to only get the next 2 chars (bottom 8 bits)
+    call    PrintHexByte        # print next 2 chars (_ _ _ _ _ _ X X)
+
+    ldw     ra, 8(sp)           # store return address for nested functions
+    ldw     r2, 4(sp)           # input value
+    ldw     r3, 0(sp)           # temp value
+    addi    sp, sp, 12          # add stack pointer
+
+    ret
+
+# ------------------------------------------------------------
+
+PrintHexWord_Attempt:
     subi    sp, sp, 20          # subtract from stack pointer
     stw     ra, 16(sp)          # store return address for nested functions
     stw     r2, 12(sp)          # used to send value to PrintHexByte
@@ -34,13 +70,13 @@ PrintHexWord:
     movi    r4, 32              # hold amount to shift right
     mov     r5, r2              # move input value to r5
 
-phw_loop:   
+phwa_loop:   
     subi    r4, r4, 8           # decrement amount to shift left by each time by 16 bits (1 byte)
     srl     r2, r5, r4          # shift value left by shift amount
     andi    r2, r2, 0xFF        # and only 8 bits (0xFF) held in r2 to be printed using printhexbyte
     call    PrintHexByte        # print the byte being held in r2 (ie. printing XX, in order of XX000000, 00XX0000, 0000XX00, 000000XX)
     subi    r3, r3, 1           # decrement loop counter
-    bgt     r3, r0, phw_loop    # loop if needed
+    bgt     r3, r0, phwa_loop    # loop if needed
 
     ldw     ra, 16(sp)          # store return address for nested functions
     ldw     r2, 12(sp)          
